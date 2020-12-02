c79a016f5eb246723e9b7e0a55c2b2399a17ec32
Jurica Zecevic <j.zecevic@emakina.hr>
2020-12-02 17:59:46 +0100

    Riješena dz

---
+++ 5.2.8_prijestupna_godina/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5._2._8_prijestupna_godina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite godinu kako bi provjerili je li prijestupna ili ne");
+            int godina = 0;
+
+            try
+            {
+                godina = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Niste unijeli ispravan format broja");
+            }
+            if ((godina % 400 == 0))
+            {
+                Console.WriteLine("Godina JE prijestupna");
+            }
+            else if ((godina % 100 == 0))
+            {
+                Console.WriteLine("Godina NIJE prijestupna");
+            }
+            else if (godina % 4 == 0)
+            {
+                Console.WriteLine("Godina JE prijestupna");
+            }
+            else
+            {
+                Console.WriteLine("Godina NIJE prijestupna");
+            }
+        }
+    }
+}
