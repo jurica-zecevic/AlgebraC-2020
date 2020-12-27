f64ade57f546a2fede493bb0b9334a720684846e
Jurica Zecevic <j.zecevic@emakina.hr>
2020-12-27 16:08:16 +0100

    Radio na zadacima

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite 10 brojeva");
+
+            List<int> brojevi = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Molim unesite {0}. broj: ", i + 1);
+                int broj = int.Parse(Console.ReadLine());
+                brojevi.Add(broj);
+            }
+
+            brojevi.Sort();
+
+            foreach (var item in brojevi)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zbroj");
+
+            Zbroj(1, 2);
+            Zbroj(10, 20);
+        }
+
+        static void Zbroj(int a, int b)
+        {
+            Console.WriteLine("Zbroj dva broja je {0}", a + b);
+        }
+    }
+}
